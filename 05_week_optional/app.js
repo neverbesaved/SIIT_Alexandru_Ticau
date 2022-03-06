90f0a8e5cf151c8551b1ba65fcc1bf1b4359d5db
Alexandru Ticau <alex.ticau2102@gmail.com>
2022-03-06 21:20:46 +0200

    Change css styling , format code

--- 05_week_optional/app.js
+++ 05_week_optional/app.js
@@ -12,48 +12,52 @@ function displayBMI(bmi) {
   let bmiWindow = document.createElement("div");
   bmiWindow.setAttribute("class", "bmi-window");
 
-  if (bmi < 18.5 && bmi>=0) {
+  if (bmi < 18.5 && bmi >= 0) {
     var bmiImage = document.createElement("img");
     bmiImage.src = "./images/underweight.png";
 
     var bmiConclusion = document.createElement("p");
-    bmiConclusion.innerText="Your bmi index is within the underweight range. Please eat more. "
+    bmiConclusion.innerText =
+      "Your bmi index is within the underweight range. Please eat more. ";
   } else if (bmi >= 18.5 && bmi < 25) {
     var bmiImage = document.createElement("img");
     bmiImage.src = "./images/normal.png";
 
     var bmiConclusion = document.createElement("p");
-    bmiConclusion.innerText="You bmi index is withing the normal range. Good job."
+    bmiConclusion.innerText =
+      "You bmi index is withing the normal range. Good job.";
   } else if (bmi >= 25 && bmi < 30) {
     var bmiImage = document.createElement("img");
     bmiImage.src = "./images/overweight.jpg";
 
     var bmiConclusion = document.createElement("p");
-    bmiConclusion.innerText="Your bmi index is within the overweight range. Practice sport and exclude junk food."
-  } else if(bmi>=30) {
+    bmiConclusion.innerText =
+      "Your bmi index is within the overweight range. Practice sport and exclude junk food.";
+  } else if (bmi >= 30) {
     var bmiImage = document.createElement("img");
     bmiImage.src = "./images/obese.jpg";
 
     var bmiConclusion = document.createElement("p");
-    bmiConclusion.innerText="Your bmi index is within the obese range. For your own health's sake - start doing physical activities and change alimentation routine "
-  } else { 
+    bmiConclusion.innerText =
+      "Your bmi index is within the obese range. For your own health's sake - start doing physical activities and change alimentation routine ";
+  } else {
     var bmiImage = document.createElement("img");
     bmiImage.src = "./images/error.jpg";
 
     var bmiConclusion = document.createElement("p");
-    bmiConclusion.innerText="An error occured, please try again "
+    bmiConclusion.innerText = "An error occured, please try again ";
   }
   let bmiValue = document.createElement("h2");
-  bmiValue.innerText=bmi;
+  bmiValue.innerText = bmi;
 
   let resetButton = document.createElement("button");
   resetButton.type = "button";
   resetButton.textContent = "Reset";
-  resetButton.setAttribute('class', 'reset-button')
-  resetButton.addEventListener('click',(event) =>{
-      bmiWindow.parentElement.removeChild(bmiWindow);
-      inputField.classList.remove("hidden");
-  })
+  resetButton.setAttribute("class", "reset-button");
+  resetButton.addEventListener("click", (event) => {
+    bmiWindow.parentElement.removeChild(bmiWindow);
+    inputField.classList.remove("hidden");
+  });
 
   bmiWindow.appendChild(bmiImage);
   bmiWindow.appendChild(bmiValue);
@@ -67,10 +71,12 @@ let bmiMain = document.getElementById("main-window");
 let weightInput = document.getElementById("weight-input");
 let heightInput = document.getElementById("height-input");
 let submitButton = document.getElementById("submit-button");
-let inputField = document.getElementById("bmi-view")
+let inputField = document.getElementById("bmi-view");
 
-submitButton.onclick = function() {
-    let newItem = displayBMI(calculateBMI(weightInput.value,heightInput.value));
-    bmiMain.appendChild(newItem);
-    inputField.classList.add("hidden");
-}
+submitButton.onclick = function () {
+  let newItem = displayBMI(calculateBMI(weightInput.value, heightInput.value));
+  bmiMain.appendChild(newItem);
+  inputField.classList.add("hidden");
+};
+
+//formatted 
