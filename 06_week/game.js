6d9fdfa8319258a6bd2a275ef157621315072c26
Alexandru Ticau <alex.ticau2102@gmail.com>
2022-03-11 01:20:05 +0200

    Fix bug , get another one more

--- 06_week/game.js
+++ 06_week/game.js
@@ -6,30 +6,34 @@ document.addEventListener("keydown", function (event) {
 function movementFunction(key) {
   let oldTop = parseInt(player.style.top);
   let oldBot = parseInt(player.style.bottom);
-  let oldRight =parseInt(player.style.right);
+  let oldRight = parseInt(player.style.right);
   let oldLeft = parseInt(player.style.left);
   switch (key) {
     case "ArrowUp":
-        if(oldBot>=0 && oldBot<=500){
-            player.style.bottom = `${oldBot + 10}px`
+      if(oldTop!==0 && oldBot!==0){
+        player.style.bottom = `${oldBot + 5}px`;
+        player.style.top = `${oldTop - 5}px`;
+        break;
+      }
+    case "ArrowLeft": 
+    if(oldRight!==0 && oldLeft!==0){
+      player.style.right = `${oldRight +   5}px`;
+      player.style.left = `${oldLeft - 5}px`;
       break;
-        }
-    case "ArrowLeft":
-        if(oldRight>=0 && oldRight<=500){
-            player.style.right = `${oldRight + 10}px`
-     }
-      break;
+    }
     case "ArrowDown":
-        if(oldTop>=0 && oldTop<=500){
-            player.style.top = `${oldTop + 10}px`
-     }
+      if(oldTop<475 && oldBot>-475){
+      player.style.top = `${oldTop + 5}px`;
+      player.style.bottom = `${oldBot - 5}px`;
       break;
+    }
     case "ArrowRight":
-        if(oldLeft>=0 && oldLeft<=500){
-            player.style.left = `${oldLeft + 10}px`
-     }
+      if(oldRight>-975 && oldLeft<975){
+      player.style.left = `${oldLeft + 5}px`;
+      player.style.right = `${oldRight - 5}px`;
       break;
-        default:
-      console.log("Input is not valid")
+  }
+    default:
+      console.log("Input is not valid");
   }
 }
