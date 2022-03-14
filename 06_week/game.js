582953963d55c4684f644df0d21491f1323c03bb
Alexandru Ticau <alex.ticau2102@gmail.com>
2022-03-14 22:40:05 +0200

    Format code

--- 06_week/game.js
+++ 06_week/game.js
@@ -2,49 +2,48 @@ createPlayground();
 
 var player = document.getElementById("player");
 var playground = document.getElementById("game-container");
-var submitButton = document.getElementById("submit-button")
-var inputs = document.getElementsByClassName("input-item")
+var submitButton = document.getElementById("submit-button");
+var inputs = document.getElementsByClassName("input-item");
 var Player = { x: 0, y: 0 };
-
-
 
 document.addEventListener("keydown", function (event) {
   keydownListener(event.key);
 });
 
-function createPlayground(){
-
+function createPlayground() {
   // 1. cream playground-ul
   let playground = document.createElement("div");
-  playground.setAttribute("id","game-container")
+  playground.setAttribute("id", "game-container");
 
   // 2. cream playerul cu o pozitie random(?) si il adaugam in playground
-  let posX = Math.floor(Math.random() * 980);
-  let posY = Math.floor(Math.random() * 480);
+
+  // let posX = Math.floor(Math.random() * 980);
+  // let posY = Math.floor(Math.random() * 480);
   let ourPlayer = document.createElement("div");
-  ourPlayer.setAttribute("id", "player")
-  ourPlayer.setAttribute("left", `${posX}px`);
-  ourPlayer.setAttribute("top", `${posY}px`);
-  playground.appendChild(ourPlayer)
+  ourPlayer.setAttribute("id", "player");
+  // ourPlayer.style.left = `${posX}px`
+  // ourPlayer.style.top = `${posY}px`
+  // Player.x = posX;
+  // Player.y = posY;
+  playground.appendChild(ourPlayer);
 
   // 3. cream numar random de obstacole , cu pozitii si dimensiuni random si le introducem pe playground
-  let numberOfObstacles = Math.floor(Math.random() * 15 +5);
-  console.log(numberOfObstacles)
-  for(i=0;i<numberOfObstacles;i++){
-  let width = Math.floor(Math.random()*70+10)
-  let height = Math.floor(Math.random()*70+10)
-  let obstaclePosX = Math.floor(Math.random() * 980);
-  let obstaclePosY = Math.floor(Math.random() * 480);
-  let obstacle = document.createElement("div");
-  obstacle.setAttribute("class", "obstacle");
-  obstacle.style.left = `${obstaclePosX}px`;
-  obstacle.style.top = `${obstaclePosY}px`;
-  obstacle.style.width = `${width}px`;
-  obstacle.style.height = `${height}px`;
-  playground.appendChild(obstacle);
+  let numberOfObstacles = Math.floor(Math.random() * 15 + 5);
+  console.log(numberOfObstacles);
+  for (i = 0; i < numberOfObstacles; i++) {
+    let width = Math.floor(Math.random() * 70 + 10);
+    let height = Math.floor(Math.random() * 70 + 10);
+    let obstaclePosX = Math.floor(Math.random() * 980);
+    let obstaclePosY = Math.floor(Math.random() * 480);
+    let obstacle = document.createElement("div");
+    obstacle.setAttribute("class", "obstacle");
+    obstacle.style.left = `${obstaclePosX}px`;
+    obstacle.style.top = `${obstaclePosY}px`;
+    obstacle.style.width = `${width}px`;
+    obstacle.style.height = `${height}px`;
+    playground.appendChild(obstacle);
   }
   document.body.appendChild(playground);
-  
 }
 function isPlayerInsideDiv(x, y) {
   if (x < 0 || playground.offsetWidth - 20 < x) {
@@ -56,14 +55,14 @@ function isPlayerInsideDiv(x, y) {
   return true;
 }
 
-function isObstacleInsideDiv(x,y, height, width) {
-  if(x <0 || playground.offsetWidth - width < x ) {
+function isObstacleInsideDiv(x, y, height, width) {
+  if (x < 0 || playground.offsetWidth - width < x) {
     return false;
   }
-  if( y<0 || playground.offsetHeight - height < y) {
+  if (y < 0 || playground.offsetHeight - height < y) {
     return false;
   }
-  return true
+  return true;
 }
 function keydownListener(key) {
   console.log(key);
@@ -97,4 +96,3 @@ function movePlayer(x, y) {
   player.style.left = `${Player.x}px`;
   player.style.top = `${Player.y}px`;
 }
-
