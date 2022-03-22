124694246b9994f1f550de5de3cd2eff01b64607
Alexandru Ticau <alex.ticau2102@gmail.com>
2022-03-22 17:50:27 +0200

    Add functionality for all buttons

--- 07_week/app.js
+++ 07_week/app.js
@@ -5,64 +5,55 @@ const saveButton = document.getElementById("save-button")
 const secondsHTML = document.getElementById("seconds");
 const minutesHTML = document.getElementById("minutes");
 const hoursHTML = document.getElementById("hours");
-let seconds = parseInt(secondsHTML.innerText); 
-let minutes = parseInt(minutesHTML.innerText); 
-let hours = parseInt(hoursHTML.innerText); 
 
-startButton.addEventListener("click", () => {
-    var myInterval = setInterval(timerFunctional,1000)
-    
-    stopButton.addEventListener("click", () => {
-    clearInterval(myInterval);
-})
-} ); 
+function startTimer(){
+  let seconds = parseInt(secondsHTML.innerText); 
+  let minutes = parseInt(minutesHTML.innerText); 
+  let hours = parseInt(hoursHTML.innerText); 
+  incrementTime = setInterval(() => {
+    seconds +=1;
 
+    if (seconds == 60) {
+      minutes +=1;
+      seconds = 0;
+    }
+  
+    if (minutes == 60) {
+      hours+=1;
+      minutes = 0;
+    }
+  
+    secondsHTML.innerHTML = addZero(seconds);
+    minutesHTML.innerHTML = addZero(minutes);
+    hoursHTML.innerHTML = addZero(hours);
+  },1000)
+}
 
+function stopTimer(){
+  clearInterval(incrementTime)
+}
 
-function timerFunctional(){
+function resetTimer(){
+  clearInterval(incrementTime)
+  secondsHTML.innerHTML = "00";
+    minutesHTML.innerHTML = "00";
+    hoursHTML.innerHTML = "00";
+}
 
-        seconds +=1;
-
-      if (seconds == 60) {
-        minutes +=1;
-        seconds = 0;
-      }
-
-      if (minutes == 60) {
-        hours+=1;
-        minutes = 0;
-      }
-
-      secondsHTML.innerHTML = addZero(seconds);
-      minutesHTML.innerHTML = addZero(minutes);
-      hoursHTML.innerHTML = addZero(hours);
+function saveTimer(){
+  let seconds = parseInt(secondsHTML.innerText); 
+  let minutes = parseInt(minutesHTML.innerText); 
+  let hours = parseInt(hoursHTML.innerText); 
+  const timerHtml = document.createElement('div');
+  timerHtml.innerHTML = `
+      <span id="hour">${addZero(hours)}</span> :
+      <span id="minute">${addZero(minutes)}</span> :
+      <span id="second">${addZero(seconds)}</span>
+  `
+  timerHtml.setAttribute("class","timer")
+  document.body.appendChild(timerHtml);
 }
 
 function addZero(value) {
     return value < 10 ? "0" + value : value
-  }
-  function createTimerHtml() {
-    const timerHtml = document.createElement('div');
-    timerHtml.innerHTML = `
-        <span id="hour">00</span> :
-        <span id="minute">00</span> :
-        <span id="secunde">00</span>
-    `
-    timerHtml.setAttribute("class","timer")
-    return timerHtml;
-  }
-  
-  function createTimer(initialSecunde) {
-    const timerHtml = createTimerHtml();
-  
-    const hourHtml = timerHtml.querySelector('#hour');
-    const minuteHtml = timerHtml.querySelector('#minute');
-    const secundeHtml = timerHtml.querySelector('#secunde');
-  
-    let secunde = initialSecunde;
-    let minute = 59;
-    let hour = 0;
-  
-    document.body.appendChild(timerHtml);
-  
   }
