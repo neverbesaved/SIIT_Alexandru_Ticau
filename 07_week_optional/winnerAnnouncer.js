41dd400e7092daccd6f126fcb6764706828d81b2
Alexandru Ticau <alex.ticau2102@gmail.com>
2022-04-06 22:28:58 +0300

    Modify styling

--- 07_week_optional/winnerAnnouncer.js
+++ 07_week_optional/winnerAnnouncer.js
@@ -9,18 +9,25 @@ const winnerAnnouncer = (computer, player) => {
   paragraphHTML.setAttribute("id","announcer")
   if (computer === player) {
     result = "It's a Draw !";
+    paragraphHTML.setAttribute("class","draw")
   } else if (computer === "Paper" && player === "Scissors") {
     result = "Player wins !";
+    paragraphHTML.setAttribute("class","win")
   } else if (computer === "Paper" && player === "Rock") {
     result = "Computer wins !";
+    paragraphHTML.setAttribute("class","lose")
   } else if (computer === "Rock" && player === "Paper") {
     result = "Player wins !";
+    paragraphHTML.setAttribute("class","win")
   } else if (computer === "Rock" && player === "Scissors") {
     result = "Computer wins !";
+    paragraphHTML.setAttribute("class","lose")
   } else if (computer === "Scissors" && player === "Paper") {
     result = "Computer wins !";
+    paragraphHTML.setAttribute("class","lose")
   } else if (computer === "Scissors" && player === "Rock") {
     result = "Player wins !";
+    paragraphHTML.setAttribute("class","win")
   }
   paragraphHTML.innerText = result;
   return paragraphHTML
