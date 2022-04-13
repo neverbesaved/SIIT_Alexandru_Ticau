d129b8cd94ace72743a39dabbb19bdbc52083fdc
Alexandru Ticau <alex.ticau2102@gmail.com>
2022-04-13 14:04:38 +0300

    Add functionality and styling for spaceships

---
+++ 10_week/app.js
@@ -0,0 +1,45 @@
+import Ship from "./ship.js";
+
+const shipList = [];
+const shipTypes = ["blue", "green", "red"];
+
+const addShip = document.getElementById("addShip");
+addShip.addEventListener("click", () => {
+  let randomNum = Math.floor(Math.random() * 3);
+  const ship = new Ship(shipTypes[randomNum], shipClick);
+  shipList.push(ship);
+});
+
+const shipClick = (activeShipLocal) => {
+  for (const ship of shipList) {
+    if (ship !== activeShipLocal) {
+      ship.stopShip = true;
+    }
+  }
+};
+
+document.addEventListener("keydown", (event) => {
+  console.log(event.key);
+  switch (event.key) {
+    case "ArrowRight":
+      shipList.forEach((ship) => {
+        ship.moveRight();
+      });
+      break;
+    case "ArrowLeft":
+      shipList.forEach((ship) => {
+        ship.moveLeft();
+      });
+      break;
+    case "ArrowUp":
+      shipList.forEach((ship) => {
+        ship.moveUp();
+      });
+      break;
+    case "ArrowDown":
+      shipList.forEach((ship) => {
+        ship.moveDown();
+      });
+      break;
+  }
+});
