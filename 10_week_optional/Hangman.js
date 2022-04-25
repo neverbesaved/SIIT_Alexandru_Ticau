dd4ccc64302fb68340b8855eae0561ba6ca5d2a8
Alexandru Ticau <alex.ticau2102@gmail.com>
2022-04-25 15:26:30 +0300

    Add lose/win functionality

--- 10_week_optional/Hangman.js
+++ 10_week_optional/Hangman.js
@@ -4,22 +4,25 @@ export class Hangman {
         let randomNumber = Math.floor(Math.random()*randomWords.length);
         let randomWord = randomWords[randomNumber];
         this.word = randomWord;
-        this.hidedWord = this.hideWord();
+        this.hiddenWord = this.hideWord();
     }
     hideWord() {
-        let hidedWord = [];
+        let hiddenWord = [];
         let myWord = Array.from(this.word);
         myWord.forEach(character => {
-            hidedWord.push("_ ");
+            hiddenWord.push("_ ");
         });
-        return hidedWord
+        return hiddenWord
     }
     guessChar(input){
+        let wrongAnswer = true;
         let myWord = Array.from(this.word);
         for(let i=0;i<myWord.length;i++){
             if(myWord[i]===input){
-                this.hidedWord[i]=input;
+                this.hiddenWord[i]=input;
+                wrongAnswer = false;
             }
         }
+        return wrongAnswer;
     }
 }
